800af1389aead012b09a297fc6ce2cf5b51cc834
james.clayton20 <1501082@uad.ac.uk>
2018-10-22 16:43:26 +0100

    - Startted developing the game object class. Trying to get it to load scene files.

--- build/vs2017/game_object.cpp
+++ build/vs2017/game_object.cpp
@@ -8,12 +8,37 @@ GameObject::GameObject()
 }
 
 
+GameObject::GameObject(gef::Platform& platform_, gef::Vector4 n_velocity, std::string n_scene_filename_)
+{
+	velocity_ = n_velocity;
+	scene_filename_ = n_scene_filename_;
+
+	Initialise(platform_);
+}
+
+
 GameObject::~GameObject()
 {
 
 }
 
-void GameObject::Update()
+void GameObject::Initialise(gef::Platform& platform_)
+{
+	// Read the specified scene file.
+	scene_.ReadSceneFromFile(platform_, scene_filename_.data());
+
+	// Extract the materials from the scene file.
+	scene_.CreateMaterials(platform_);
+
+	// If there's mesh data in the scene, generate a mesh from the scene file.
+	if (scene_.mesh_data.size() > 0)
+	{
+		mesh_ = scene_.CreateMesh(platform_, scene_.mesh_data.front());
+	}
+
+}
+
+void GameObject::UpdatePosition()
 {
 	if (velocity_.Length() > 0.0f)
 	{
@@ -23,3 +48,6 @@ void GameObject::Update()
 
 
 }
+
+
+
--- build/vs2017/game_object.h
+++ build/vs2017/game_object.h
@@ -1,6 +1,13 @@
 #pragma once
 
 #include <graphics/mesh_instance.h>
+#include <graphics/scene.h>
+#include <string>
+
+namespace gef
+{
+	class Platform;
+}
 
 
 class GameObject : gef::MeshInstance
@@ -8,12 +15,22 @@ class GameObject : gef::MeshInstance
 
 public:
 	GameObject();
+	GameObject
+	(
+		gef::Platform& platform_,
+		gef::Vector4 n_velocity,
+		std::string n_scene_filename_
+	);
 	~GameObject();
 
-	void Update();
+	void UpdatePosition();
+	void Initialise(gef::Platform& platform_);
 
-protected:
+	void SetVelocity(gef::Vector4 n_velocity) { velocity_ = n_velocity; }
+
+private:
 	gef::Vector4 velocity_;
-	
+	std::string scene_filename_;
+	gef::Scene scene_;
 };
 
