73d826b370ce77949118f03be572ff119434fb05
james.clayton20 <1501082@uad.ac.uk>
2018-11-30 20:56:18 +0000

    - Fleshed out Button class. Still does nothing.

--- build/vs2017/Button.cpp
+++ build/vs2017/Button.cpp
@@ -10,3 +10,44 @@ Button::Button()
 Button::~Button()
 {
 }
+
+bool Button::Init(gef::Platform& platform, gef::Vector4 pos, std::string text)
+{
+	button_texture_ = CreateTextureFromPNG("button_texture", platform);
+
+	if (button_texture_)
+	{
+		button_sprite_.set_height(button_height_);
+		button_sprite_.set_width(button_width_);
+		button_sprite_.set_position(button_position_);
+	}
+
+	button_text_ = text;
+
+	button_position_ = pos;
+
+	return false;
+}
+
+
+gef::Vector4 Button::GetPosition()
+{
+	return button_position_;
+}
+
+void Button::SetPosition(gef::Vector4 n_position)
+{
+	button_position_ = n_position;
+}
+
+void Button::SetPosition(float x, float y, float z)
+{
+	button_position_.set_x(x);
+	button_position_.set_y(y);
+	button_position_.set_z(z);
+}
+
+gef::Sprite * Button::GetSprite()
+{
+	return &button_sprite_;
+}
--- build/vs2017/Button.h
+++ build/vs2017/Button.h
@@ -2,6 +2,10 @@
 #include "maths\vector4.h"
 #include "graphics\sprite.h"
 #include "graphics\texture.h"
+#include <string>
+#include "m:\AppliedGamesTechnology\ar_app\load_texture.h"
+
+class gef::Platform;
 
 class Button
 {
@@ -9,10 +13,23 @@ public:
 	Button();
 	~Button();
 
+	bool Init(gef::Platform& platform, gef::Vector4 pos, std::string text);
+
+	gef::Vector4 GetPosition();
+	void SetPosition(gef::Vector4 n_position);
+	void SetPosition(float x, float y, float z);
+
+	gef::Sprite* GetSprite();
+
 private:
-	gef::Vector4 position_;
+	gef::Vector4 button_position_;
 	gef::Texture* button_texture_;
 	gef::Sprite button_sprite_;
+
+	float button_width_;
+	float button_height_;
+
+	std::string button_text_;
 	
 };
 
